9960db4f4888684c59576a9d3777f6117839c8d9
kellycesario <kellycesario@gmail.com>
2022-09-16 10:14:19 -0300

    Developing the component Background (tsx and style)

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,19 @@
+import { ImageBackground } from "react-native";
+import { styles } from "./styles";
+
+import backgroundImage from "../../assets/background-galaxy.png";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground 
+    source={backgroundImage} 
+    defaultSource={backgroundImage}
+    style={styles.container}>
+      {children}
+    </ImageBackground>
+  );
+}
