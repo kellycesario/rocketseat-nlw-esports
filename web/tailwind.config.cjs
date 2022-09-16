d5bee7073a7a237823bdbb13f399e8715ce0d444
kellycesario <kellycesario@gmail.com>
2022-09-15 21:03:32 -0300

    Adding fontFamily - inter, sans-serif to the project

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage:{
         galaxy: "url('./assets/images/background-galaxy.png')",
